2a9578016867e39d3fa3493477b6bfb746481519
Surya Hendra Kurniawan <suhekukuliah@gmail.com>
2026-09-19 00:24:57 +0800

    tugas

--- posttest/posttest-apd-2/2609106011-SURYA HENDRA KURNIAWAN-PT-2.py
+++ posttest/posttest-apd-2/2609106011-SURYA HENDRA KURNIAWAN-PT-2.py
@@ -8,29 +8,31 @@ usd = 17740
 ringgit = 4326
 
 barang = [barang_1, barang_2, barang_3, barang_4, barang_5, barang_6]
+print("List barang         :", barang)
 
 total_belanja = barang[0] + barang[1] + barang[2] + barang[3] + barang[4] + barang[5]
+print("Total belanja       :", total_belanja)
 
 pajak = total_belanja * 0.15
+print("Pajak 15%           :", pajak)
 
 total_bayar = total_belanja + pajak
+print("Total bayar         :", total_bayar)
 
 rata_rata = total_bayar / len(barang)
+print("Rata-rata           :", rata_rata)
 
 nim = 11
+print("NIM                 :", nim)
 
 bolean = nim < rata_rata
-
-total_belanja_usd = total_belanja / usd
-total_belanja_ringgit = total_belanja / ringgit
-
-print("List barang         :", barang)
-print("Total belanja       :", total_belanja)
-print("Pajak 15%           :", pajak)
-print("Total bayar         :", total_bayar)
-print("Rata-rata           :", rata_rata)
-print("NIM                 :", nim)
 print("Bolean              :", bolean)
-print("Total dalam USD     :$", total_belanja_usd)
-print("Total dalam RINGGIT :RM", total_belanja_ringgit)
+
+total_bayar_usd = total_bayar / usd
+print("Total dalam USD     :$", total_bayar_usd)
+
+total_bayar_ringgit = total_bayar / ringgit
+print("Total dalam RINGGIT :RM", total_bayar_ringgit)
+ 
 print("Barang 1, 3, 5      :", barang[0:5:2])
+#bang, mba ini saya pakai tab supaya rapih di bagian "printnya" bukan karena AI :( 
